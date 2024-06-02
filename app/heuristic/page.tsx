93be5f77ae360ee5722c0d58d8c7597920471112
Andy Warr <andywarr@gmail.com>
2024-06-02 15:04:28 -0700

    Added trial

--- app/heuristic/page.tsx
+++ app/heuristic/page.tsx
@@ -1,12 +1,17 @@
 import { auth } from "@/auth";
 import { redirect } from 'next/navigation'
 import { SignOut } from "../components/sign-out";
+import { StartTrial } from "../lib/data";
 
 export default async function Heuristic() {
   const session = await auth();
   
   if (!session) {
     redirect("/");
+  }
+
+  if (session.user?.id) {
+    StartTrial(session.user?.id);
   }
 
   return (
