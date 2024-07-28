0cfd8a04cd595551717921be5934261864f54a46
Andy Warr <andywarr@gmail.com>
2024-07-27 17:11:33 -0700

    Added a Link to the header

--- app/heuristic/layout.tsx
+++ app/heuristic/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { SignOut } from "@/app/components/sign-out";
 
 export default function RootLayout({
@@ -9,7 +10,7 @@ export default function RootLayout({
       <div>
         <header>
           <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-            <div><span className="font-black">Seer </span>Heuristic Evaluation</div>
+            <div><span className="font-black">Seer </span><Link href="/heuristic">Heuristic Evaluation</Link></div>
             <SignOut />
           </div>
         </header>
