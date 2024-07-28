994b0e6f136d9b5e9f9cd674506c6aca7a690e44
Andy Warr <andywarr@gmail.com>
2024-07-27 17:09:21 -0700

    Added a header

--- app/heuristic/[id]/page.tsx
+++ app/heuristic/[id]/page.tsx
@@ -30,65 +30,71 @@ export default async function Page({ params }: { params: { id: string } }) {
   const TABLE_HEAD = ["Heuristic", "Violated", "Reason"];
 
   return (
-  <Card className="container mx-auto mb-6 h-full w-full overflow-scroll">
-    <table className="w-full table-auto text-left">
-        <thead>
-          <tr>
-          {TABLE_HEAD.map((head) => (
-              <th
-                key={head}
-                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-              >
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
+  <main className="container mx-auto px-4 py-6">
+    <Typography
+      className="mb-4"
+      variant="h5"
+    >Results</Typography>
+    <Card className="container mx-auto mb-6 h-full w-full overflow-scroll">
+      <table className="w-full table-auto text-left">
+          <thead>
+            <tr>
+            {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                 >
-                  {head}
-                </Typography>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {heuristicEvaluation.results.map(({id, heuristic, violated, reason}, index) => {
-            const isLast = index === heuristicEvaluation.results.length - 1;
-            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {heuristicEvaluation.results.map(({id, heuristic, violated, reason}, index) => {
+              const isLast = index === heuristicEvaluation.results.length - 1;
+              const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
-            return (
-              <tr key={id} className="even:bg-blue-gray-50/50">
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {heuristic}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {violated === 'yes' ? 'Yes' : 'No'}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {reason}
-                  </Typography>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-    </table>
-  </Card>
+              return (
+                <tr key={id} className="even:bg-blue-gray-50/50">
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {heuristic}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {violated === 'yes' ? 'Yes' : 'No'}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {reason}
+                    </Typography>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+      </table>
+    </Card>
+  </main>
   )
 }
