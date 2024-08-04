de174f86346b0a2c83d52676c4d9988195a9cfaa
Andy Warr <andywarr@gmail.com>
2024-08-04 15:30:04 -0700

    Removed unused imports

--- app/components/heuristic-evaluation-form.tsx
+++ app/components/heuristic-evaluation-form.tsx
@@ -6,8 +6,7 @@ import {
 import { Evaluate } from "@/app/components/evaluate-button";
 import { heuristicEvaluationFormAction } from "@/app/lib/action";
 import { useRef, useState } from 'react';
-import { z, ZodIssue } from "zod";
-import Heuristic from "../heuristic/new/page";
+import { z } from "zod";
 
 interface PropData {
   credits: number;
@@ -35,13 +34,15 @@ const heuristicEvaluationSchema = z.object({
   }).refine((files) => files.every((file) => file.size > 0), "At least one image file must be uploaded."),
   heuristic: z.union([z.literal("nielsen"), z.literal("tenets")])
 })
- 
-export function HeuristicEvaluationForm(props:  {user: User} ) {
-  const [errors, setErrors] = useState({fieldErrors : {
-    goal: [],
-    files: [],
-    heuristics: []
-  }});
+
+export function HeuristicEvaluationForm(props: { user: User }) {
+  const [errors, setErrors] = useState({
+    fieldErrors: {
+      goal: [],
+      files: [],
+      heuristics: []
+    }
+  });
 
   const heuristicEvaluationFormActionPreProcessing = async (formData: FormData) => {
     const newHeuristicEvaluation = {
@@ -63,7 +64,7 @@ export function HeuristicEvaluationForm(props:  {user: User} ) {
     const heuristicEvaluationFormActionWithId = heuristicEvaluationFormAction.bind(null, props.user);
     await heuristicEvaluationFormActionWithId(formData);
   }
-  
+
 
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [files, setFiles] = useState(0);
@@ -74,7 +75,7 @@ export function HeuristicEvaluationForm(props:  {user: User} ) {
     fileInputRef.current.click();
   };
 
-  const handleFileInputChange = (e : any) => {
+  const handleFileInputChange = (e: any) => {
     setFiles(e.target.files.length);
   };
 
@@ -92,34 +93,34 @@ export function HeuristicEvaluationForm(props:  {user: User} ) {
         color="red"
         className="h-[21px] mt-2 flex items-center gap-1 font-normal"
       >{errors.fieldErrors.goal && errors.fieldErrors.goal.length > 0 ? errors.fieldErrors.goal[0] : ''}</Typography>
-      
+
       <div className="flex">
         <input
           accept="images/*"
           className="hidden"
-          multiple={true} 
+          multiple={true}
           name="file"
           onChange={handleFileInputChange}
           ref={fileInputRef}
           type="file" />
 
-        <Button 
+        <Button
           className="flex items-center gap-3 mt-6"
           onClick={handleButtonClick}
           variant="gradient">
           <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-5 w-5"
           >
-              <path
+            <path
               strokeLinecap="round"
               strokeLinejoin="round"
               d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-              />
+            />
           </svg>
           Upload Flow
         </Button>
@@ -127,32 +128,32 @@ export function HeuristicEvaluationForm(props:  {user: User} ) {
         <p className="flex flex-wrap content-end ml-3 gap-3 mt-4"><span className="antialiased block font-light text-xs">{files} {files !== 1 ? ' files ' : ' file '} selected.</span></p>
       </div>
       <Typography
-          variant="small"
-          color="red"
-          className="h-[21px] mt-2 mb-6 flex items-center gap-1 font-normal"
-        >{errors.fieldErrors.files && errors.fieldErrors.files.length > 0 ? errors.fieldErrors.files[0] : ''}</Typography>
+        variant="small"
+        color="red"
+        className="h-[21px] mt-2 mb-6 flex items-center gap-1 font-normal"
+      >{errors.fieldErrors.files && errors.fieldErrors.files.length > 0 ? errors.fieldErrors.files[0] : ''}</Typography>
 
       <Typography
         color="blue-gray">
         Which set of heuristics would you like to evaluate the flow?
       </Typography>
 
-      <Radio 
+      <Radio
         defaultChecked
         label="Nielsen"
         name="heuristic"
         value="nielsen" crossOrigin={undefined} />
 
-      <Radio 
+      <Radio
         label="Tenents & Traps"
         name="heuristic"
-        value="tenets" crossOrigin={undefined}  />
+        value="tenets" crossOrigin={undefined} />
       <Typography
         variant="small"
         color="red"
         className="h-[21px] mt-2 flex items-center gap-1 font-normal"
       >{errors.fieldErrors.heuristics && errors.fieldErrors.heuristics.length > 0 ? errors.fieldErrors.heuristics[0] : ''}</Typography>
-      
+
       <div className="flex">
         <Evaluate credits={props.user.credits} />
         <p className="flex flex-wrap content-end ml-3"><span className="antialiased block font-light text-xs">{props.user.credits} {props.user.credits !== 1 ? 'tries' : 'try'} remaining.</span></p>
