6caa49878f8aab42a0a0adb977485da34bb05417
Andy Warr <andywarr@gmail.com>
2024-08-04 22:21:59 -0700

    Added Google Analytics

--- app/heuristic/layout.tsx
+++ app/heuristic/layout.tsx
@@ -2,19 +2,19 @@ import Link from 'next/link'
 import { SignOut } from "@/app/components/sign-out";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div>
-        <header>
-          <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-            <div><span className="font-black">Seer </span><Link href="/heuristic">Heuristic Evaluation</Link></div>
-            <SignOut />
-          </div>
-        </header>
-        {children}
-      </div>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <header>
+        <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+          <div><span className="font-black">Seer </span><Link href="/heuristic">Heuristic Evaluation</Link></div>
+          <SignOut />
+        </div>
+      </header>
+      {children}
+    </div>
+  );
+}
