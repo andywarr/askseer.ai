b5be05732a19337f5685d05559f506e280c070f6
Andy Warr <andywarr@gmail.com>
2024-08-23 14:42:36 -0700

    Uploaded the spinner color

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -7,7 +7,7 @@ import {
 export function Loading() {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen bg-black/80 flex items-center justify-center ">
-      <Spinner className="h-16 w-16 text-white" />
+      <Spinner color="blue-gray" className="h-16 w-16" />
     </div>
   )
 }
