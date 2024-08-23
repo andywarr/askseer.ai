b86715111c854372495479029ac35f5a379108e0
Andy Warr <andywarr@gmail.com>
2024-08-22 21:10:55 -0700

    Updated the loading UI

--- app/components/evaluate-button.tsx
+++ app/components/evaluate-button.tsx
@@ -3,19 +3,22 @@
 import {
   Button
 } from "@/MTailwind";
+import { Loading } from "@/app/components/loading";
 import { useFormStatus } from 'react-dom'
 
 export function Evaluate(props: { credits: number }) {
   const { pending } = useFormStatus();
 
   return (
-    <Button
-      disabled={props.credits > 0 && !pending ? false : true}
-      loading={pending}
-      variant="gradient"
-      size="md"
-      className="w-32"
-      type="submit"
-    >Evaluate</Button>
+    <div>
+      <Button
+        disabled={props.credits > 0 && !pending ? false : true}
+        variant="gradient"
+        size="md"
+        className="w-32"
+        type="submit"
+      >Evaluate</Button>
+      {pending && <Loading />}
+    </div>
   )
 }
