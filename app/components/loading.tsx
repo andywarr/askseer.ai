cbfb955cbeb06c77343a6e2f683963c903b3b629
Andy Warr <andywarr@gmail.com>
2024-10-17 15:24:35 -0700

    Added loading pane above the sidebar

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -4,7 +4,7 @@ import { Spinner } from "@/MTailwind";
 
 export function Loading() {
   return (
-    <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-black/80">
+    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black/80">
       <Spinner color="blue-gray" className="h-16 w-16" />
     </div>
   );
