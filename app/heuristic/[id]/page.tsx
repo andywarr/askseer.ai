df503b0bc2db6a44adee330a8736835843a58737
Andy Warr <andywarr@gmail.com>
2024-10-22 20:28:06 -0700

    Added a more menu

--- app/heuristic/[id]/page.tsx
+++ app/heuristic/[id]/page.tsx
@@ -1,5 +1,13 @@
 import { redirect } from "next/navigation";
-import { Card, Typography } from "@/MTailwind";
+import {
+  Card,
+  IconButton,
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+  Typography,
+} from "@/MTailwind";
 import { getHeuristicEvaluation } from "@/app/lib/data";
 import Image from "next/image";
 import { isAuthenticated } from "@/app/lib/dal";
@@ -32,9 +40,36 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <main className="container mx-auto px-4 py-6">
-      <Typography className="mb-4" variant="h5">
-        {heuristicEvaluation.userGoal}
-      </Typography>
+      <div className="mb-4 flex">
+        <div className="flex-grow">
+          <Typography variant="h5" className="flex h-full items-center">
+            {heuristicEvaluation.userGoal}
+          </Typography>
+        </div>
+        <div className="ml-4 flex">
+          <Menu placement="bottom-end">
+            <MenuHandler>
+              <IconButton variant="text" ripple={false}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" />
+                  <path d="M14 20C14 21.1046 13.1046 22 12 22C10.8954 22 10 21.1046 10 20C10 18.8954 10.8954 18 12 18C13.1046 18 14 18.8954 14 20Z" />
+                  <path d="M14 4C14 5.10457 13.1046 6 12 6C10.8954 6 10 5.10457 10 4C10 2.89543 10.8954 2 12 2C13.1046 2 14 2.89543 14 4Z" />
+                </svg>
+              </IconButton>
+            </MenuHandler>
+            <MenuList className="flex flex-col gap-2">
+              <MenuItem disabled>Share</MenuItem>
+              <MenuItem>Delete</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </div>
+
       <div className="mb-8 flex max-h-64 flex-nowrap items-center justify-between gap-4 overflow-x-auto">
         {presignedUrls.map((url, index) => (
           <div className="max-w-full flex-grow shadow" key={index}>
