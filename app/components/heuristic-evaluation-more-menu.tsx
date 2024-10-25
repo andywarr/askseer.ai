cdd98e781acf8d9d1be0c19ef65c66fc1b4dc445
Andy Warr <andywarr@gmail.com>
2024-10-24 22:52:09 -0700

    Updated the heuristic evaluation page UI

--- app/components/heuristic-evaluation-more-menu.tsx
+++ app/components/heuristic-evaluation-more-menu.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { IconButton, Menu, MenuHandler, MenuItem, MenuList } from "@/MTailwind";
 import { deleteHeuristicEvaluation } from "../lib/data";
 import { deleteS3Objects } from "../lib/action";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function MoreMenu({
   heuristicEvaluation,
@@ -35,25 +42,30 @@ export default function MoreMenu({
   };
 
   return (
-    <Menu placement="bottom-end">
-      <MenuHandler>
-        <IconButton variant="text" ripple={false}>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
+            height="h-4"
             viewBox="0 -960 960 960"
-            width="24px"
+            width="h-4"
             fill="currentColor"
-            className="h-6 w-6"
           >
             <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
           </svg>
-        </IconButton>
-      </MenuHandler>
-      <MenuList className="flex flex-col gap-2">
-        <MenuItem disabled>Share</MenuItem>
-        <MenuItem onClick={handleDelete}>Delete</MenuItem>
-      </MenuList>
-    </Menu>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="bottom" align="end">
+        <DropdownMenuGroup>
+          <DropdownMenuItem disabled>
+            <span>Share</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleDelete}>
+            <span>Delete</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
