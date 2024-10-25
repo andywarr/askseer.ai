aed02708a9212df8a00dfe7e2fb66a120adc48b8
Andy Warr <andywarr@gmail.com>
2024-10-24 18:27:27 -0700

    Removed unused imports

--- app/heuristic/page.tsx
+++ app/heuristic/page.tsx
@@ -7,10 +7,8 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { isAuthenticated } from "../lib/dal";
 import { getPresignedUrls } from "../lib/action";
