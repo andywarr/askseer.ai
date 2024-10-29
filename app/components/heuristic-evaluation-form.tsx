6dca63c5df07e28702d680d1d61f1f15f03c8a24
Andy Warr <andywarr@gmail.com>
2024-10-28 20:25:03 -0700

    Refactor SVG size and loading animation in HeuristicEvaluationForm and Loading components

--- app/components/heuristic-evaluation-form.tsx
+++ app/components/heuristic-evaluation-form.tsx
@@ -278,7 +278,7 @@ export function HeuristicEvaluationForm(props: { credits: number }) {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        className="mx-auto h-6 w-6"
+                        className="mx-auto h-4 w-4"
                         strokeWidth={2}
                         stroke="currentColor"
                       >
