203c7613507be1d9557eeb1e079dec08f4be4240
Andy Warr <andywarr@gmail.com>
2024-10-31 21:39:23 -0700

    Updated schema

--- app/util/migration.mjs
+++
@@ -1,7 +0,0 @@
-import { PrismaClient as OldPrisma } from "@prisma/client";
-import { PrismaClient as NewPrisma } from "@prisma/client";
-
-async function migrateDatabases() {
-  const oldPrisma = new OldPrisma();
-  const newPrisma = new NewPrisma();
-}
