3812bfc46d427dc2ef8d2a8d836dcf23f40ed077
Andy Warr <andywarr@gmail.com>
2025-01-06 17:58:24 -0800

    Updated the Prisma schema and removed obsolete migration files and associated SQL commands

--- prisma/migrations/20241224225512_added_context/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "HeuristicEvaluation" ADD COLUMN     "context" TEXT;
--- prisma/migrations/20250104000250_updated_question1_type/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `question1` column on the `CWStepDetail` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "CWStepDetail" DROP COLUMN "question1",
-ADD COLUMN     "question1" BOOLEAN;
