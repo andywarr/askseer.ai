6ecf3abc99ce457ffa347febf4d616233cf696e1
Andy Warr <andywarr@gmail.com>
2025-06-10 08:48:18 -0700

    Refactor Home component layout for improved structure and readability

--- apps/nextjs-app/app/(no-auth)/page.tsx
+++ apps/nextjs-app/app/(no-auth)/page.tsx
@@ -8,8 +8,12 @@ import { auth } from "@/apps/nextjs-app/auth";
 // Component imports
 import { GoogleSignIn } from "@/apps/nextjs-app/components/google-sign-in";
 import { ResendSignIn } from "@/apps/nextjs-app/components/resend-sign-in";
+
 // UI component imports
 import { Separator } from "@/apps/nextjs-app/components/ui/separator";
+
+// Custom components
+import { GlobalHeader } from "@/apps/nextjs-app/components/global-header";
 
 export default async function Home() {
   const session = await auth();
@@ -19,26 +23,31 @@ export default async function Home() {
   }
 
   return (
-    <div className="flex min-h-screen animate-gradient items-center bg-gradient-to-r from-red-400 via-pink-500 to-blue-500 bg-[length:400%_400%] p-8">
-      <div className="w-full min-w-80 max-w-max p-2 text-white">
-        <h1 className="mb-4 text-8xl drop-shadow-lg">Seer</h1>
-        <h2 className="mb-2 text-2xl">AI-assisted research</h2>
-        <p className="mb-6 max-w-xs text-sm">
-          Save hours on research with the click of a button
-        </p>
-        <ResendSignIn />
-        <Separator className="mt-4" />
-        <GoogleSignIn />
-        <p className="mt-4 max-w-xs text-sm">
-          By clicking the sign in button you agree to our{" "}
-          <Link className="underline" href={"/privacy"}>
-            Privacy Policy
-          </Link>{" "}
-          and{" "}
-          <Link className="underline" href={"/terms"}>
-            Terms of Service
-          </Link>
-        </p>
+    <div className="min-h-screen w-full animate-gradient bg-gradient-to-r from-red-400 via-pink-500 to-blue-500 bg-[length:400%_400%]">
+      <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center p-8">
+        <GlobalHeader activePage="home" primaryCta="signIn" theme="dark" />
+        <div className="mt-16 w-full min-w-80 max-w-max p-2 text-white">
+          {/* <h1 className="mb-4 text-8xl drop-shadow-lg">Seer</h1> */}
+          <h3 className="mt-16 scroll-m-20 text-2xl font-semibold tracking-tight">
+            AI-Assisted Research
+          </h3>
+          <p className="mb-8 mt-8 max-w-xs leading-7 [&:not(:first-child)]:mt-6">
+            Save hours on research with the click of a button
+          </p>
+          <ResendSignIn />
+          <Separator />
+          <GoogleSignIn />
+          <p className="mt-4 max-w-xs text-sm">
+            By clicking the sign in button you agree to our{" "}
+            <Link className="underline" href={"/privacy"}>
+              Privacy Policy
+            </Link>{" "}
+            and{" "}
+            <Link className="underline" href={"/terms"}>
+              Terms of Service
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
