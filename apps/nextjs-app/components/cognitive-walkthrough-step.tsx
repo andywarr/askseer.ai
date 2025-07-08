97d8a0ce4c8e660e10b5e5a32b9087a63d87cdba
Andy Warr <warr@askseer.ai>
2025-07-08 15:39:53 -0700

    Refactor CognitiveWalkthroughStep layout for improved readability and structure

--- apps/nextjs-app/components/cognitive-walkthrough-step.tsx
+++ apps/nextjs-app/components/cognitive-walkthrough-step.tsx
@@ -20,60 +20,57 @@ export function CognitiveWalkthroughStep(props: {
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
         Step {props.step} of {props.totalSteps}
       </h3>
-      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="col-span-1">
-          <Image
-            src={props.imageUrl}
-            alt={`Step ${props.step} in the user flow`}
-            width={500}
-            height={500}
-            priority={true}
-            unoptimized={true}
-            className="mx-auto h-auto w-full border object-contain p-1 shadow md:mx-0"
-          />
-        </div>
-        <div className="col-span-1 mt-4 w-full min-w-0 space-y-4 text-sm md:mt-0">
-          <div className="flex flex-col gap-4">
-            {props.step > 1 && (
-              <div>
-                <p className="text-xs leading-7 tracking-tight text-zinc-500">
-                  Is the user interface at this step what was expected?
-                </p>
-                <p className="leading-7">{props.expected ? "Yes" : "No"}</p>
-              </div>
-            )}
-            <div>
-              {props.step < props.totalSteps && (
-                <div>
-                  <p className="text-sm leading-7 tracking-tight text-zinc-500">
-                    {props.results[0].question.question}
-                  </p>
-                  <p className="leading-7">{props.results[0].answer}</p>
-                </div>
-              )}
-            </div>
-            <div>
-              {props.step < props.totalSteps && (
-                <div>
-                  <p className="text-sm leading-7 tracking-tight text-zinc-500">
-                    {props.results[1].question.question}
-                  </p>
-                  <p className="leading-7">{props.results[1].answer}</p>
-                </div>
-              )}
-            </div>
-            <div>
-              {props.step < props.totalSteps && (
-                <div>
-                  <p className="text-sm leading-7 tracking-tight text-zinc-500">
-                    {props.results[2].question.question}
-                  </p>
-                  <p className="leading-7">{props.results[2].answer}</p>
-                </div>
-              )}
-            </div>
+      <div className="mb-4">
+        <Image
+          src={props.imageUrl}
+          alt={`Step ${props.step} in the user flow`}
+          width={500}
+          height={500}
+          priority={true}
+          unoptimized={true}
+          className="mx-auto mb-4 h-auto w-full border object-contain p-1 shadow md:float-left md:mr-4 md:w-1/2"
+        />
+        {props.step > 1 && (
+          <div className="mb-4 mt-4 md:mt-0">
+            <p className="text-xs leading-7 tracking-tight text-zinc-500">
+              Is the user interface at this step what was expected?
+            </p>
+            <p className="text-sm leading-7 tracking-tight">
+              {props.expected ? "Yes" : "No"}
+            </p>
           </div>
-        </div>
+        )}
+        {props.step < props.totalSteps && (
+          <div className="mb-4">
+            <p className="text-xs leading-7 tracking-tight text-zinc-500">
+              {props.results[0].question.question}
+            </p>
+            <p className="text-sm leading-7 tracking-tight">
+              {props.results[0].answer}
+            </p>
+          </div>
+        )}
+        {props.step < props.totalSteps && (
+          <div className="mb-4">
+            <p className="text-xs leading-7 tracking-tight text-zinc-500">
+              {props.results[1].question.question}
+            </p>
+            <p className="text-sm leading-7 tracking-tight">
+              {props.results[1].answer}
+            </p>
+          </div>
+        )}
+        {props.step < props.totalSteps && (
+          <div className="mb-4">
+            <p className="text-xs leading-7 tracking-tight text-zinc-500">
+              {props.results[2].question.question}
+            </p>
+            <p className="text-sm leading-7 tracking-tight">
+              {props.results[2].answer}
+            </p>
+          </div>
+        )}
+        <div className="clear-both"></div>
       </div>
       <div>
         <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
