a6fa71597d91f4f208f4db220d7b97ba79446091
Andy Warr <warr@askseer.ai>
2025-07-08 11:26:11 -0700

    Refactor CognitiveWalkthroughDetails to display discoverability issues

--- apps/nextjs-app/components/cognitive-walkthrough-details.tsx
+++ apps/nextjs-app/components/cognitive-walkthrough-details.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 // Next imports
 import Image from "next/image";
 
 // Ui component imports
-import { AspectRatio } from "@/apps/nextjs-app/components/ui/aspect-ratio";
+import { InfoCard } from "@/apps/nextjs-app/components/info-card";
 
 export function CognitiveWalkthroughDetails(props: {
   step: number;
@@ -12,7 +14,7 @@ export function CognitiveWalkthroughDetails(props: {
   issues: any;
   imageUrl: string;
 }) {
-  console.log(props.results);
+  console.log(props.issues);
 
   return (
     <div className="mb-8 flex w-full flex-col gap-2">
@@ -31,11 +33,11 @@ export function CognitiveWalkthroughDetails(props: {
             className="mx-auto h-auto w-full border object-contain p-1 shadow md:mx-0"
           />
         </div>
-        <div className="col-span-1 mt-4 w-full min-w-0 space-y-4 md:mt-0">
+        <div className="col-span-1 mt-4 w-full min-w-0 space-y-4 text-sm md:mt-0">
           <div className="flex flex-col gap-4">
             {props.step > 1 && (
               <div>
-                <p className="text-sm leading-7 tracking-tight text-zinc-500">
+                <p className="text-xs leading-7 tracking-tight text-zinc-500">
                   Is the user interface at this step what was expected?
                 </p>
                 <p className="leading-7">{props.expected ? "Yes" : "No"}</p>
@@ -80,67 +82,26 @@ export function CognitiveWalkthroughDetails(props: {
         </h4>
       </div>
       <div>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">None</p>
+        {props.issues
+          .filter((issue: any) => issue.issueType === "DISCOVERABILITY")
+          .map((issue: any) => (
+            <InfoCard
+              key={issue.id}
+              id={issue.id}
+              studyType="cognitiveWalkthrough"
+              type="issue"
+              content={issue.issue}
+              source={issue.source}
+            />
+          ))}
+        {props.issues.filter(
+          (issue: any) => issue.issueType === "DISCOVERABILITY",
+        ).length === 0 && (
+          <p className="text-sm text-zinc-500">
+            No discoverability issues found.
+          </p>
+        )}
       </div>
     </div>
-
-    // <div className="mb-4 flex w-full flex-col gap-2">
-    //   <div className="font-semibold leading-7 tracking-tight">
-    //     Step {props.step} of {props.totalSteps}
-    //   </div>
-    //   <div className="flex flex-row gap-4">
-    //     <div className="w-[200px]">
-    //       <AspectRatio ratio={1 / 1}>
-    //         <Image
-    //           src={props.imageUrl}
-    //           alt={`Step ${props.step} in the user flow`}
-    //           width={200} // Placeholder width
-    //           height={200} // Placeholder height
-    //           className="rounded-md object-cover p-1 shadow"
-    //         />
-    //       </AspectRatio>
-    //     </div>
-    //     <div className="flex flex-col gap-4">
-    //       {props.step > 1 && (
-    //         <div>
-    //           <p className="text-sm leading-7 tracking-tight text-zinc-500">
-    //             Is the user interface at this step what was expected?
-    //           </p>
-    //           <p className="leading-7">{props.expected ? "Yes" : "No"}</p>
-    //         </div>
-    //       )}
-    //       <div>
-    //         {props.step < props.totalSteps && (
-    //           <div>
-    //             <p className="text-sm leading-7 tracking-tight text-zinc-500">
-    //               {props.results[0].question.question}
-    //             </p>
-    //             <p className="leading-7">{props.results[0].answer}</p>
-    //           </div>
-    //         )}
-    //       </div>
-    //       <div>
-    //         {props.step < props.totalSteps && (
-    //           <div>
-    //             <p className="text-sm leading-7 tracking-tight text-zinc-500">
-    //               {props.results[1].question.question}
-    //             </p>
-    //             <p className="leading-7">{props.results[1].answer}</p>
-    //           </div>
-    //         )}
-    //       </div>
-    //       <div>
-    //         {props.step < props.totalSteps && (
-    //           <div>
-    //             <p className="text-sm leading-7 tracking-tight text-zinc-500">
-    //               {props.results[2].question.question}
-    //             </p>
-    //             <p className="leading-7">{props.results[2].answer}</p>
-    //           </div>
-    //         )}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
