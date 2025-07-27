f73c30f0781b6ed523b707a9231570062aca41c1
Andy Warr <warr@askseer.ai>
2025-07-27 13:51:31 -0700

    refactor: update service name in logger for consistency across applications

--- apps/db-worker/src/logger.ts
+++ apps/db-worker/src/logger.ts
@@ -20,7 +20,7 @@ const baseLogger = pino({
     log(object) {
       return {
         ...object,
-        service: "seer-web", // add a static field
+        service: "seer-db", // add a static field
       };
     },
   },
